ad403ffea88a3e32f62d23c5e28bd3683ca6e948
rcosven <rcosven@gmail.com>
2026-07-09 12:49:07 -0400

    a

--- start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# Arranque Railway: siempre ejecuta el script del working dir /app.
-set -e
-echo "[start.sh] BOT arrancando $(date -u +%Y-%m-%dT%H:%M:%SZ)"
-exec python translate_missing.py
