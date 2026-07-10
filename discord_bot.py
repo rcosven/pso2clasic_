3a5d88113932db4bf575d5a398014c6f04323e89
rcosven <rcosven@gmail.com>
2026-07-10 08:11:10 -0400

    Update discord_bot.py

--- discord_bot.py
+++ discord_bot.py
@@ -15,10 +15,14 @@ CARPETAS = {
 
 class BuscadorBot(commands.Bot):
     def __init__(self):
-        # Creamos los intents y habilitamos message_content
         intents = discord.Intents.default()
-        intents.message_content = True  # <--- ESTA LÍNEA ES CLAVE
+        intents.message_content = True
         super().__init__(command_prefix="!", intents=intents)
+
+    async def setup_hook(self):
+        # Limpia comandos antiguos globalmente y registra los actuales
+        await self.tree.sync() 
+        print("Comandos sincronizados con Discord.")
 
 bot = BuscadorBot()
 
